4ad41e31365a7826d3ca11dc790805d52de8ce53
sathishpandiyan126@gmail.com <sathishpandiyan126@gmail.com>
2024-11-04 18:25:39 +0530

    new2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from './Components/Header'; // Ensure correct casing
+import { Helmet } from 'react-helmet';
+import Header from './Components/Header';
 import About from './Components/About';
 import Events from './Components/Events';
 import Workshops from './Components/Workshops';
@@ -7,11 +8,16 @@ import Team from './Components/Team';
 import Contact from './Components/Contact';
 import FAQ from './Components/FAQ';
 import Footer from './Components/Footer';
-import ScrollToTopButton from './Components/ScrollToTopButton'; // Import the button
+import ScrollToTopButton from './Components/ScrollToTopButton';
 
 function App() {
   return (
     <div className="App font-sans">
+      <Helmet>
+        <title>PEA-SME</title>
+        <meta name="description" content="Production Engineering Association (PEA) & Society of Manufacturing Engineers (SME) - Student Chapters,PSG College Of Technology,Coimbatore" />
+        <meta name="keywords" content="PEA,SME,PSG TECH,Production," />
+      </Helmet>
       <Header />
       <main>
         <About />
@@ -22,7 +28,7 @@ function App() {
         <FAQ />
       </main>
       <Footer />
-      <ScrollToTopButton /> {/* Add the button here */}
+      <ScrollToTopButton />
     </div>
   );
 }
